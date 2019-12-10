1a0e0ba1dd1e0306052d86c0cb68a3bb62f2f1ae
fatuk <fatukk@gmail.com>
2019-12-10 18:21:17 +0300

    Update server

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send(`<h1>Hello ${req.query.name}</h1>`);
+  res.send(`<h1>Hello ${req.query.name}</h1><p>Test</p>`);
 });
 
 module.exports = router;
